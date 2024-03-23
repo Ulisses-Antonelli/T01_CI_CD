896161d31f359a5654c320328c9ba4831aede54f
ulisses-antonelli <ulisses.antonelli@fatec.sp.gov.br>
2024-03-23 09:00:43 -0300

    arquivo modificado

--- T01_CI_CD.java
+++ T01_CI_CD.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("CÓDIGO INICIAL");
+        System.out.println("Hello, World!");
+        printCustomMessage("This is a custom message!");
+    }
+    
+    public static void printCustomMessage(String message) {
+        System.out.println(message);
     }
 }
